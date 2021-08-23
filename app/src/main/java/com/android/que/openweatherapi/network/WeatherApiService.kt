0843463a2fc533a28c763b94f82250bf6cee1f92
justinrichmond.que <justinrichmond.que@mynt.xyz>
2021-08-23 21:59:13 +0800

    Fixed bugs

--- app/src/main/java/com/android/que/openweatherapi/network/WeatherApiService.kt
+++ app/src/main/java/com/android/que/openweatherapi/network/WeatherApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface WeatherApiService {
     //KELVIN
-//    https://api.openweathermap.org/data/2.5/weather?q=manila&appid=611c78ec258e14f9d2e8abb545af11d5
+//    https://api.openweathermap.org/data/2.5/weather?q=manila&appid=YOURAPIKEY
     @GET("data/2.5/weather?")
     fun getCurrentWeatherData(
         @Query("q") name: String,
@@ -14,7 +14,7 @@ interface WeatherApiService {
     ): Call<WeatherResponse>
 
     //FAHRENHEIT
-    //    https://api.openweathermap.org/data/2.5/weather?q=manila&units=imperial&appid=611c78ec258e14f9d2e8abb545af11d5
+    //    https://api.openweathermap.org/data/2.5/weather?q=manila&units=imperial&appid=YOURAPIKEY
     @GET("data/2.5/weather?")
     fun getCurrentWeatherDataInFahrenheit(
         @Query("q") name: String,
@@ -24,7 +24,7 @@ interface WeatherApiService {
 
 
     //CELSIUS
-//    https://api.openweathermap.org/data/2.5/weather?q=manila&units=metric&appid=611c78ec258e14f9d2e8abb545af11d5
+//    https://api.openweathermap.org/data/2.5/weather?q=manila&units=metric&appid=YOURAPIKEY
     @GET("data/2.5/weather?")
     fun getCurrentWeatherDataInCelsius(
         @Query("q") name: String,
